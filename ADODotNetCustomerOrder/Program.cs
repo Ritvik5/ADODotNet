c816c57d6e27f34d846d0f55f9ee99265a62e455
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-11 16:16:54 +0530

    [Ritvik]Add: Welcome Message

---
+++ ADODotNetCustomerOrder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADODotNetCustomerOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To ADO.net");
+        }
+    }
+}
