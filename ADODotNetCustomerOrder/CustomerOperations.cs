3072f013f3a9cb22bf32c7e72b0e761c8b13b35c
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-11 22:48:00 +0530

    [Ritvik]Add: Adding Details in Customer and Orders Table using Transactions

--- ADODotNetCustomerOrder/CustomerOperations.cs
+++ ADODotNetCustomerOrder/CustomerOperations.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ namespace ADODotNetCustomerOrder
 
         public static void CheckConnection()
         {
-            using(SqlConnection con1 = new SqlConnection("data source = (localdb)\\MSSQLLocalDB; initial catalog = CustomerDB; integrated security = true "))
+            using (SqlConnection con1 = new SqlConnection("data source = (localdb)\\MSSQLLocalDB; initial catalog = CustomerDB; integrated security = true "))
             {
                 con.Open();
                 Console.WriteLine("Connection Established Successfully");
@@ -34,8 +35,9 @@ namespace ADODotNetCustomerOrder
             catch (Exception e)
             {
 
-                Console.WriteLine("Somethiing went Wrong."+e);
-            }finally
+                Console.WriteLine("Somethiing went Wrong." + e);
+            }
+            finally
             {
                 con.Close();
             }
@@ -45,17 +47,17 @@ namespace ADODotNetCustomerOrder
         {
             try
             {
-                using(con)
+                using (con)
                 {
                     Customer model = new Customer();
                     string query = "SELECT * FROM Customer";
                     SqlCommand command = new SqlCommand(query, con);
                     con.Open();
                     SqlDataReader reader = command.ExecuteReader();
-                    if(reader.HasRows)
+                    if (reader.HasRows)
                     {
                         Console.WriteLine("-----------Data-------------");
-                        while(reader.Read())
+                        while (reader.Read())
                         {
                             model.Customer_Id = Convert.ToInt32(reader["Customer_Id"]);
                             model.Customer_Name = Convert.ToString(reader["Customer_Name"]);
@@ -65,7 +67,7 @@ namespace ADODotNetCustomerOrder
                             model.Pincode = Convert.ToString(reader["Pincode"]);
                             model.Salary = Convert.ToDecimal(reader["Salary"]);
                             Console.WriteLine("Customer_Id: {0}\nCustomer_Name: {1}\nPhoneNumber: {2}\nAddress: {3}\nCountry: {4}\nPincode: {5}\nSalary: {6}\n",
-                                               model.Customer_Id,model.Customer_Name,model.PhoneNumber,model.Address,model.Country,model.Pincode,model.Salary);
+                                               model.Customer_Id, model.Customer_Name, model.PhoneNumber, model.Address, model.Country, model.Pincode, model.Salary);
                         }
                     }
                     return true;
@@ -74,7 +76,7 @@ namespace ADODotNetCustomerOrder
             }
             catch (Exception e)
             {
-                Console.WriteLine("Something went Wrong"+e);
+                Console.WriteLine("Something went Wrong" + e);
                 return false;
             }
             finally
@@ -103,5 +105,95 @@ namespace ADODotNetCustomerOrder
                 con.Close();
             }
         }
+
+        public static void UpdateIntoTable()
+        {
+            try
+            {
+                string query = "UPDATE Customer SET Address = 'Chandigarh' WHERE Customer_Id = 5";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Data Updated into the table.");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Somethiing went Wrong." + e);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static void InsertCustomerTableUsingTransaction()
+        {
+            using (con)
+            {
+                con.Open();
+                SqlTransaction transaction = con.BeginTransaction();
+
+                try
+                {
+                    string query = "INSERT INTO Customer VALUES('Smith','111111111','Church Street','India','560055',36000.00)";
+                    SqlCommand cmd = new SqlCommand(query,con,transaction);
+                    cmd.ExecuteNonQuery();
+
+                    string query1 = "INSERT INTO Customer VALUES('Racheal','888888888','Kormangla','India','560055',66000.00)";
+                    cmd = new SqlCommand(query1,con,transaction);
+                    cmd.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    Console.WriteLine("Transaction Committed");
+
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine("Transaction RollBack");
+                }
+                finally
+                {
+                    con.Close( );
+                }
+            }
+
+        }
+
+        public static void InsertOrdersTableUsingTransaction()
+        {
+            using (con)
+            {
+                con.Open();
+                SqlTransaction transaction = con.BeginTransaction();
+
+                try
+                {
+                    string query = "INSERT INTO Orders(ProductName,Quantity,Rating,Customer_Id) VALUES('Product A','5',3,6)";
+                    SqlCommand cmd = new SqlCommand(query, con, transaction);
+                    cmd.ExecuteNonQuery();
+
+                    string query1 = "INSERT INTO Orders(ProductName,Quantity,Rating,Customer_Id) VALUES('Product C','10',4,9)";
+                    cmd = new SqlCommand(query1, con, transaction);
+                    cmd.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    Console.WriteLine("Transaction Committed");
+
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine("Transaction RollBack" + e.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+        }
+
     }
 }
--- ADODotNetCustomerOrder/Program.cs
+++ ADODotNetCustomerOrder/Program.cs
@@ -5,12 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To ADO.net\n");
+            Customer custbj = new Customer();
 
             //CustomerOperations.CheckConnection();
             //CustomerOperations.InsertIntoTable();
-            CustomerOperations.DeleteRecordFromTable();
+            //CustomerOperations.DeleteRecordFromTable();
+            //CustomerOperations.UpdateIntoTable();
             CustomerOperations obj = new CustomerOperations();
             //obj.Display();
+            //obj.InsertUsingStoredProcedure(custbj);
+            //CustomerOperations.InsertCustomerTableUsingTransaction();
+            CustomerOperations.InsertOrdersTableUsingTransaction();
         }
     }
 }
